6a2ce1b9f01e4006a24fff1695e6412148f47af8
derhub <imjohnderbaul@gmail.com>
2026-07-03 05:52:37 +0800

    refactor!: delete zero-consumer sdk and logger packages

--- packages/sdk/src/index.ts
+++
@@ -1,4 +0,0 @@
-export type * from "./types/index";
-export { AtError } from "./types/index";
-export * from "./protocol/index";
-export type { DaemonTransport, FileSource, FrameHandler, HookSource } from "./ports";
